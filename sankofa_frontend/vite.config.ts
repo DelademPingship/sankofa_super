4c276b747d345f2e39cee69ef239be51157a368f
Sama <etornamasamoah@gmail.com>
2025-11-30 06:26:59 +0000

    Vite Update

--- sankofa_frontend/vite.config.ts
+++ sankofa_frontend/vite.config.ts
@@ -1,23 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import { viteImagemin } from 'vite-plugin-imagemin';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      mozjpeg: { quality: 85 },
-      pngquant: { quality: [0.8, 0.9] },
-      svgo: {
-        plugins: [
-          { name: 'removeViewBox', active: false },
-          { name: 'removeEmptyAttrs', active: false }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
     host: true
